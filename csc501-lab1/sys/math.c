bd357d3a06e936260ea0bb264900c5a49395cd93
jsdeokar <jsdeokar@ncsu.edu>
2019-09-13 19:34:08 -0400

    new folders

---
+++ csc501-lab1/sys/math.c
@@ -0,0 +1,38 @@
+
+double log(double x){
+
+	int n = 1;
+	double val = 0.0;
+
+	for (;n<=20;n++){
+
+		val = val - ((pow(x,n))/n);
+	}	
+
+	return val;
+}
+
+double pow(double x, int y){
+	
+	int i = 0;
+	double prod = 1.0;
+
+	for(;i<y;i++){
+
+		prod = prod*x;
+	}
+
+	return prod;
+}
+
+
+double expdev(double lambda){
+
+	double dummy;
+    	do
+        	dummy = (double)rand() / RAND_MAX;
+    	while (dummy == 0.0);
+
+    	return -(log(dummy) / lambda);
+
+}
